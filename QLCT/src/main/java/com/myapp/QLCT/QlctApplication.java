ed8bc2876a595d79a31a6067b9a6eb5dccf9b93f
tran mau <mautranvo44@gmail.com>
2025-04-02 19:09:49 +0700

    khoi tao

---
+++ QLCT/src/main/java/com/myapp/QLCT/QlctApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.QLCT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlctApplication.class, args);
+	}
+
+}
---
+++ QLCT/src/test/java/com/myapp/QLCT/QlctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.QLCT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
