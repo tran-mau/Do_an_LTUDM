3b042cbfcf2f083a89a34f2b64bc5a6cf8c62c01
luong <tranluong1680@gmail.com>
2025-04-03 17:05:45 +0700

    test 03/04/2025

--- QLCT/src/main/java/com/myapp/QLCT/QlctApplication.java
+++ QLCT/src/main/java/com/myapp/QLCT/QlctApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QlctApplication {
 
 	public static void main(String[] args) {
+		String m = "hello";
 		SpringApplication.run(QlctApplication.class, args);
 	}
 
