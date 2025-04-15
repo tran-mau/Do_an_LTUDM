e76fcdb10e6cdb8e54c6c303c8e8f41b33bb626d
ipinnxitin <thangtran882110@gmail.com>
2025-04-15 17:23:29 +0700

    Components structure for expense management

---
+++ qlct-ui/src/app/pages/income-outcome/income-outcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-income-outcome',
+  imports: [],
+  templateUrl: './income-outcome.component.html',
+  styleUrl: './income-outcome.component.css'
+})
+export class IncomeOutcomeComponent {
+
+}
---
+++ qlct-ui/src/app/shared/menu-side/menu-side.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-side',
+  imports: [],
+  templateUrl: './menu-side.component.html',
+  styleUrl: './menu-side.component.css'
+})
+export class MenuSideComponent {
+
+}
