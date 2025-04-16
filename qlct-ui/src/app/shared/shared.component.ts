fe59e133a2c5b06c52e64cb884dd5aeed74f91f8
luong <tranluong1680@gmail.com>
2025-04-16 08:20:43 +0700

    luong2

--- qlct-ui/src/app/shared/shared.component.ts
+++ qlct-ui/src/app/shared/shared.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SettingComponent } from '../pages/setting/setting.component';
+import { ContentComponent } from '../components/content-dashboard/content-dashboard.component';
 
 @Component({
   selector: 'app-shared',
   imports: [
-    SettingComponent
+    SettingComponent,
+    ContentComponent
   ],
   templateUrl: './shared.component.html',
   styleUrl: './shared.component.css'
 })
-export class SharedComponent {
+export class SharedComponent implements OnInit {
+  activeSection: string = 'home';
+  
+  ngOnInit(): void {
+    this.setupMenuListeners();
+  }
+  
+  setupMenuListeners() {
+    const menuItems = document.querySelectorAll('.nav-item');
 
+    menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+        this.activeSection = item.getAttribute('data-section') || 'home';
+        menuItems.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+      });
+    });
+  }
 }
